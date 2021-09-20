1f7a7c357a125bb82592b2a87561309b4fcb534e
lideming <me@yuuza.net>
2021-09-20 10:59:06 +0800

    added api

---
+++ src/api/api.ts
@@ -0,0 +1,179 @@
+// file: api.ts
+
+import { Callbacks, Action, CancelToken, sleepAsync } from "@yuuza/webfx";
+import { Api } from "./apidef";
+
+
+export const api = new class {
+  readonly baseUrl = "https://mc.yuuza.net/api/";
+  readonly appBaseUrl = "https://mc.yuuza.net/";
+  storageUrlBase = '';
+  debugSleep = 0;
+  defaultAuth: string | null = null;
+
+  onTrackInfoChanged = new Callbacks<Action<Api.Track>>();
+  onTrackDeleted = new Callbacks<Action<Api.Track>>();
+
+  async _fetch(input: RequestInfo, init?: RequestInit) {
+    if (this.debugSleep) await sleepAsync(this.debugSleep * (Math.random() + 1));
+    return await fetch(input, {
+      credentials: 'same-origin',
+      ...init
+    });
+  }
+  getHeaders(arg: FetchOptions) {
+    arg = arg || {};
+    var headers: any = {};
+    var auth = arg.auth ?? this.defaultAuth;
+    if (auth) headers['Authorization'] = auth;
+    if (arg.cache != true) headers['Cache-Control'] = 'no-store';
+    return headers;
+  }
+  async get(path: string, options?: FetchOptions): Promise<any> {
+    options = options || {};
+    var resp = await this._fetch(this.baseUrl + path, {
+      headers: { ...this.getHeaders(options) }
+    });
+    await this.checkResp(options, resp);
+    return await resp.json();
+  }
+  async post(arg:
+    { method?: 'POST' | 'PUT' | 'DELETE'; }
+    & PostOptions & PostBodyOptions
+  ) {
+    var body = arg.obj;
+    if (arg.mode === undefined) arg.mode = body !== undefined ? 'json' : 'empty';
+    if (arg.mode === 'json') body = body !== undefined ? JSON.stringify(body) : undefined;
+    else if (arg.mode === 'raw') void 0; // noop
+    else if (arg.mode === 'empty') body = null;
+    else throw new Error('Unknown arg.mode');
+
+    var headers = this.getHeaders(arg);
+    if (arg.mode === 'json') headers['Content-Type'] = 'application/json';
+
+    headers = { ...headers, ...arg.headers };
+
+    var resp = await this._fetch(this.baseUrl + arg.path, {
+      body: body,
+      method: arg.method ?? 'POST',
+      headers: headers
+    });
+    await this.checkResp(arg, resp);
+    var contentType = resp.headers.get('Content-Type');
+    if (contentType && /^application\/json;?/.test(contentType))
+      return await resp.json();
+    return null;
+  }
+  put(arg: PostOptions & PostBodyOptions) {
+    return this.post({ ...arg, method: 'PUT' });
+  }
+  delete(arg: PostOptions) {
+    return this.post({ ...arg, method: 'DELETE' });
+  }
+  upload(arg: {
+    method: 'POST' | 'PUT';
+    url: string;
+    body: any;
+    auth?: string;
+    contentType?: string;
+    cancelToken?: CancelToken;
+    onprogerss?: (e: ProgressEvent) => void;
+  }) {
+    const ct = arg.cancelToken;
+    if (ct) {
+      var cb = ct.onCancelled.add(function () {
+        xhr.abort();
+      });
+    }
+
+    const xhr = new XMLHttpRequest();
+    const whenXhrComplete = new Promise<void>((resolve, reject) => {
+      xhr.onload = ev => resolve();
+      xhr.onerror = ev => reject("XHR error");
+      xhr.onabort = ev => reject("XHR abort");
+    });
+    xhr.upload.onprogress = arg.onprogerss || null;
+
+    xhr.open(arg.method, this.processUrl(arg.url));
+
+    if (arg.auth) xhr.setRequestHeader('Authorization', arg.auth);
+    if (arg.contentType) xhr.setRequestHeader('Content-Type', arg.contentType);
+
+    xhr.send(arg.body);
+    const complete = (async function (checkStatus?: boolean) {
+      try {
+        await whenXhrComplete;
+      } finally {
+        if (ct) {
+          ct.onCancelled.remove(cb!);
+          ct.throwIfCancelled();
+        }
+      }
+
+      if (checkStatus === undefined || checkStatus)
+        if (xhr.status < 200 || xhr.status >= 300) throw new Error("HTTP status " + xhr.status);
+      return xhr;
+    })();
+
+    return {
+      xhr,
+      complete
+    };
+  }
+  private async checkResp(options: { status?: number | false; }, resp: Response) {
+    if (options.status !== false &&
+      ((options.status !== undefined && resp.status != options.status)
+        || resp.status >= 400)) {
+      if (resp.status === 450) {
+        try {
+          var resperr = (await resp.json()).error;
+        }
+        catch { }
+        if (resperr)
+          throw new Error(resperr);
+      }
+      throw new Error('HTTP status ' + resp.status);
+    }
+  }
+  async getListAsync(id: number): Promise<Api.TrackListGet> {
+    return await this.get('lists/' + id);
+  }
+  async getListIndexAsync(): Promise<Api.TrackListIndex> {
+    return await this.get('lists/index');
+  }
+  async putListAsync(list: Api.TrackListPut, creating: boolean = false): Promise<Api.TrackListPutResult> {
+    return await this.post({
+      path: 'lists/' + list.id,
+      method: creating ? 'POST' : 'PUT',
+      obj: list,
+    });
+  }
+  getTrack(id: number) { return this.get('tracks/' + id) as Promise<Api.Track>; }
+  getList(id: number) { return this.get('lists/' + id) as Promise<Api.Track>; }
+  processUrl(url: string) {
+    if (url.match('^(https?:/)?/')) return url;
+    if (this.storageUrlBase && url.startsWith('storage/'))
+      return this.storageUrlBase + url.substr(8);
+    return this.baseUrl + url;
+  }
+};
+
+export interface PostOptions extends FetchOptions {
+  path: string;
+  headers?: Record<string, string>;
+}
+
+export interface FetchOptions {
+  status?: number;
+  auth?: string;
+  cache?: boolean;
+}
+
+export type PostBodyOptions =
+  {
+    mode?: 'json' | 'raw';
+    obj: any,
+  } | {
+    mode?: 'empty';
+    obj?: undefined;
+  };
---
+++ src/api/apidef.d.ts
@@ -0,0 +1,189 @@
+// file: apidef.d.ts
+
+export namespace Api {
+  /** 
+   * GET {api}/lists/index
+   */
+  interface TrackListIndex {
+    lists: TrackListInfo[];
+  }
+
+  interface TrackListInfo {
+    id: number;
+    owner: number;
+    name: string;
+    visibility: number;
+    version: number;
+  }
+
+  /**
+   * GET {api}/lists/{id}
+   */
+  interface TrackListGet extends TrackListInfo {
+    tracks: Track[];
+  }
+
+  /**
+   * [Create list]
+   * POST {api}/lists/-
+   * 
+   * [Update list]
+   * PUT {api}/lists/{id}
+   */
+  interface TrackListPut extends TrackListInfo {
+    trackids?: number[];
+  }
+
+  interface TrackListPutResult extends TrackListInfo {
+
+  }
+
+  /**
+   * GET {api}/tracks/{id}
+   */
+  interface Track {
+    id: number;
+    owner?: number;
+    name: string;
+    artist: string;
+    album?: string;
+    albumArtist?: string;
+    /** URL to audio file */
+    url: string;
+    picurl?: string;
+    lyrics?: string;
+    size?: number;
+    length?: number;
+    groupId?: number;
+    files?: TrackFile[];
+    version?: number;
+    visibility?: number;
+  }
+
+  interface TrackFile {
+    profile: string;
+    /** Bitrate in kbps */
+    bitrate: number;
+    format: string;
+    size: number;
+  }
+
+  /**
+   * GET {api}/tracks/{id}/lyrics
+   */
+  interface TrackLyrics {
+    lyrics: string;
+  }
+
+  /** 
+   * [Get info of existing user]
+   * GET {api}/users/me
+   * GET {api}/users/{id}
+   * GET {api}/users/{username}
+   * 
+   * [Update info of existing user]
+   * PUT {api}/users/me
+   * 
+   * [Register a new user]
+   * POST {api}/users/new
+   */
+  interface UserInfo {
+    username: string;
+
+    /** When GET */
+    id?: number;
+    /** When GET */
+    lists?: TrackListInfo[];
+
+    /** When PUT */
+    listids?: number[];
+
+    /** When register */
+    passwd?: string;
+
+    /** When GET me */
+    playing?: Api.TrackLocation;
+
+    /** When GET me */
+    role?: 'admin' | 'user';
+
+    /** When GET me */
+    serverOptions?: ServerOptions;
+
+    /** When GET me/login */
+    token?: string;
+  }
+
+  interface ServerOptions {
+    msg?: string;
+    storageUrlBase?: string;
+    notesEnabled?: boolean;
+    discussionEnabled?: boolean;
+    trackCommentsEnabled?: boolean;
+  }
+
+  interface Error {
+    error: string;
+  }
+
+  // GET {api}/comments/{key}?[begin={id}][end={id}][limit={}]
+  interface CommentList {
+    comments: Comment[];
+    /** Zero or undefined means no more comments */
+    next: number;
+  }
+
+  interface Comment {
+    id: number;
+    uid: number;
+    username: string;
+    date: string;
+    content: string;
+  }
+
+  // GET/POST {api}/users/me/playing
+  interface TrackLocation {
+    listid: number;
+    position: number;
+    trackid: number;
+    track?: Track;
+    profile?: string;
+  }
+
+  // POST {api}/tracks/uploadrequest
+  // request:
+  interface UploadRequest {
+    filename: string;
+    size: number;
+  }
+  // response:
+  type UploadParameters = {
+    mode: 'direct';
+  } | {
+    mode: 'put-url';
+    url: string;
+    method: 'POST' | 'PUT';
+    tag: string;
+  };
+
+  // POST {api}/tracks/uploadresult
+  // request:
+  interface UploadResult {
+    url: string;
+    filename: string;
+    tag: string;
+  }
+  // response: Api.Track
+
+  // POST {api}/tracks/visibility
+  // request:
+  interface VisibilityChange {
+    trackids: number[];
+    visibility: number;
+  }
+
+  // GET {api}/tracks/group/{id}
+  interface TrackGroup {
+    tracks: Track;
+  }
+}
