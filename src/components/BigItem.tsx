bfc1ea240ce55d896e976146903e07cca993c179
lideming <me@yuuza.net>
2021-10-05 17:27:02 +0800

    fix: BigItem style

--- src/components/BigItem.tsx
+++ src/components/BigItem.tsx
@@ -20,13 +20,16 @@ export const BigItem = React.memo((props: Props) => (
     }}>
     <View style={{ flexDirection: 'row' }}>
       <Image
-        style={{ height: 68, width: 68, borderRadius: 16 }}
+        style={{ height: 68, width: 68, borderRadius: 16, backgroundColor: '#DDDDDD' }}
         source={{ uri: props.pic }}
       />
       <View style={{ marginLeft: 24, flex: 1 }}>
-        <Text style={{ fontSize: 16, marginTop: 8 }}>{props.title}</Text>
+        <Text style={{ fontSize: 16, marginTop: 8 }} numberOfLines={1} ellipsizeMode="tail">
+          {props.title}
+        </Text>
         <Text
-          style={{ fontSize: 16, marginTop: 8, color: 'rgba(0,0,0,.24)' }}>
+          style={{ fontSize: 16, marginTop: 8, color: 'rgba(0,0,0,.24)' }}
+          numberOfLines={1} ellipsizeMode="tail">
           {props.subtitle}
         </Text>
       </View>
