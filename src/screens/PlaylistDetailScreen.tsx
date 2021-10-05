500747d1b111eac69160c0fd95d0c4084d38cc91
lideming <me@yuuza.net>
2021-10-05 17:55:11 +0800

    feat: PlaylistDetail header

--- src/screens/PlaylistDetailScreen.tsx
+++ src/screens/PlaylistDetailScreen.tsx
@@ -1,10 +1,11 @@
 
 import { useRoute } from '@react-navigation/core';
 import React from 'react';
-import { Image, StatusBar, Text, View, FlatList } from 'react-native';
+import { Image, StatusBar, Text, View, FlatList, StyleSheet } from 'react-native';
 import { useTheme } from 'react-native-paper';
 import { usePlayList } from '../api';
 import { BigItem } from '../components/BigItem';
+import { arraySum } from '../api/webfxUtil';
 
 const PlaylistDetailScreen = React.memo(() => {
   const { params } = useRoute() as { params: { id: number } }
@@ -16,31 +17,53 @@ const PlaylistDetailScreen = React.memo(() => {
     return { ...track, key: track.id + '_' + keysurfix };
   });
   console.info('list', list.id, tracks?.length);
+  const minutes = !tracks ? 0 : Math.round(arraySum(tracks, x => x.length) / 60);
 
   return (
     <View style={{ flex: 1 }}>
       <View style={{ backgroundColor: theme.colors.primary, padding: 24 }}>
-        <Image style={{ width: 112, height: 112, borderRadius: 16 }}
-          source={{ uri: list.picurl }} />
-        <Text>{list.id}</Text>
-        <Text>{list.name}</Text>
-        <Text>{list.ownerName}</Text>
+        <View style={{ flexDirection: 'row' }}>
+          <Image style={{ width: 112, height: 112, borderRadius: 16 }}
+            source={{ uri: list.picurl }} />
+          <View style={{ marginLeft: 24 }}>
+            <Text style={styles.title}>{list.name}</Text>
+            <Text style={styles.subtitle}>{list.state == 'loading' ? "加载中……" : (`${tracks?.length} 首，${minutes} 分钟`)}</Text>
+            <Text style={styles.subtitle}>{list.ownerName}</Text>
+          </View>
+        </View>
       </View>
       <FlatList
         data={tracks}
         renderItem={({ item }) => {
           return <BigItem key={item.key} title={item.name} subtitle={item.artist} pic={item.picurl!} />;
         }}
-        contentContainerStyle={{
-          backgroundColor: '#F4F4F4',
-          padding: 16,
-          paddingVertical: 24,
-          paddingBottom: 180,
-        }}
+        contentContainerStyle={styles.contentBox}
       />
     </View>
   );
 });
+
+const styles = StyleSheet.create({
+  title: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: '700',
+    lineHeight: 24,
+    marginBottom: 4,
+  },
+  subtitle: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    lineHeight: 24,
+    marginBottom: 4,
+  },
+  contentBox: {
+    backgroundColor: '#F4F4F4',
+    padding: 16,
+    paddingVertical: 24,
+    paddingBottom: 180,
+  }
+})
 
 
 export default PlaylistDetailScreen;
