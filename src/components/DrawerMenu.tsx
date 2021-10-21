0ff6170b52d67013ac881cbee3fab368722a7d7f
fronz <z233@msn.com>
2021-10-22 00:36:10 +0800

    refactor: drawer item

--- src/components/DrawerMenu.tsx
+++ src/components/DrawerMenu.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 import { View, StatusBar, Text } from 'react-native';
-import { Drawer } from 'react-native-paper';
+import { Drawer, TouchableRipple } from 'react-native-paper';
 import { useNavigation, CommonActions } from '@react-navigation/native';
+
+interface DrawerItemProps {
+  label: string;
+  onPress?: () => void;
+}
+
+const DrawerItem = (props: DrawerItemProps) => {
+  return (
+    <TouchableRipple
+      style={{
+        borderRadius: 8,
+      }}
+      onPress={props.onPress}
+      borderless={true}
+      rippleColor="rgba(0, 0, 0, .2)">
+      <View
+        style={{
+          padding: 12,
+          borderRadius: 8,
+        }}>
+        <Text
+          style={{
+            fontSize: 16,
+          }}>
+          {props.label}
+        </Text>
+      </View>
+    </TouchableRipple>
+  );
+};
 
 const DrawerMenu = () => {
   const [active, setActive] = React.useState('');
@@ -16,28 +46,33 @@ const DrawerMenu = () => {
 
   return (
     <Drawer.Section
-      title="MusicCloud"
       style={{
         paddingTop: StatusBar.currentHeight,
         borderWidth: 0,
         flex: 1,
       }}>
+      <Text
+        style={{
+          paddingHorizontal: 20,
+          paddingTop: 16,
+          fontSize: 16,
+          fontWeight: 'bold',
+        }}>
+        MusicCloud
+      </Text>
       <View
         style={{
           flex: 1,
           justifyContent: 'space-between',
         }}>
         <View>
-          <Drawer.Item
-            label="设置"
-            active={active === 'setting'}
-            onPress={() => navigateTo('设置')}
-          />
-          <Drawer.Item
-            label="关于"
-            active={active === 'about'}
-            onPress={() => navigateTo('关于')}
-          />
+          <View
+            style={{
+              padding: 8,
+            }}>
+            <DrawerItem label="设置" onPress={() => navigateTo("设置")} />
+            <DrawerItem label="关于" onPress={() => navigateTo("关于")} />
+          </View>
         </View>
       </View>
     </Drawer.Section>
--- src/components/RippleOverlay.tsx
+++ src/components/RippleOverlay.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { View } from "react-native";
-import { TouchableRipple } from "react-native-paper";
+import React from 'react';
+import { View } from 'react-native';
+import { TouchableRipple } from 'react-native-paper';
 
 export const RippleOverlay = (props: { onPress?: () => void }) => (
-    <TouchableRipple onPress={props.onPress} rippleColor="rgba(0, 0, 0, .2)" style={{ position: 'absolute', width: '100%', height: '100%' }}>
-        <View></View>
-    </TouchableRipple>
-)
+  <TouchableRipple
+    onPress={props.onPress}
+    rippleColor="rgba(0, 0, 0, .2)"
+    style={{ position: 'absolute', width: '100%', height: '100%' }}>
+    <View></View>
+  </TouchableRipple>
+);
