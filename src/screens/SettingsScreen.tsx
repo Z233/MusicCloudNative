fed2ae2bb34f8c3c9b5df703ecb5063811512873
fronz <z233@msn.com>
2021-10-24 01:58:25 +0800

    feat: theme color frame

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import layout from '../styles/layout';
+import { List, useTheme, Portal, Modal } from 'react-native-paper';
 
-const SettingsScreen = () => (
-  <View>
-    <Text>Settings screen.</Text>
-  </View>
-);
+const SettingsScreen = () => {
+  const theme = useTheme();
 
-export default SettingsScreen
+  const [visible, setVisible] = React.useState(false);
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = { backgroundColor: 'white', padding: 20 };
+  return (
+    <>
+      <View style={layout.container}>
+        <List.Item
+          title="主题色"
+          onPress={() => showModal()}
+          right={() => (
+            <View
+              style={{
+                justifyContent: 'center',
+              }}>
+              <View
+                style={{
+                  height: 24,
+                  width: 24,
+                  backgroundColor: theme.colors.primary,
+                  borderRadius: 999,
+                }}
+              />
+            </View>
+          )}
+        />
+      </View>
+      <Portal>
+        <Modal
+          visible={visible}
+          onDismiss={hideModal}
+          contentContainerStyle={containerStyle}>
+          
+        </Modal>
+      </Portal>
+    </>
+  );
+};
+
+export default SettingsScreen;
